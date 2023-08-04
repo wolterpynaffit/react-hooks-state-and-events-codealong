2f28a2265d6d3e0d72882608381232115e7af140
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-08-03 20:03:41 -0400

    toggle

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import React from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [display, setDisplay] = useState(false)
+
+  function toggle(){
+    setDisplay(!display)
+  }
+  
+  return <button style = {{background : display ? 'red' : 'white'}}onClick={toggle}> {display ? 'On' : 'Off'}</button>;
 }
 
 export default Toggle;
